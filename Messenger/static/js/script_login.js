0628790d6f147f053ea9c7600626b0f0615989eb
NataliD22 <146338455+NataliD22@users.noreply.github.com>
2023-12-29 21:40:49 +0300

    Update script_login.js

--- Messenger/static/js/script_login.js
+++ Messenger/static/js/script_login.js
@@ -9,3 +9,39 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const formEL = document.getElementById('registrationForm');
+                formEL.addEventListener('submit', event => {
+                    event.preventDefault();
+
+                    const formData = new FormData(formEL);
+                    const data = Object.fromEntries(formData);
+
+                    fetch('http://localhost:8080/user/create', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    }).then(res => res.json())
+                      .then(data => console.log(data))
+                      .catch(error => console.log(error));
+                });
+
+const formE = document.getElementById('loginform');
+                formE.addEventListener('submit', event => {
+                    event.preventDefault();
+
+                    const formData = new FormData(formE);
+                    const data = Object.fromEntries(formData);
+
+                    fetch('http://localhost:8080/user/login', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    }).then(res => res.json())
+                      .then(data => console.log(data))
+                      .catch(error => console.log(error));
+                });
